f7eebde9686b62ef9059d2c6ec95285e912732da
Big Malc <joshuaeric.malcolm@gmail.com>
2020-03-10 12:03:44 +1300

    Script Fix

--- Project/Assets/Scripts/WingsuitController.cs
+++ Project/Assets/Scripts/WingsuitController.cs
@@ -10,10 +10,15 @@ public class WingsuitController : MonoBehaviour
     // Rotation
     private Vector3 rot;
 
-    // Min speed, when the player is on 0 deg or whatever minAngle you have
-    public float lowSpeed = 12.5f;
-    // Max speed
-    public float highSpeed = 13.8f;
+    public float MaxLowSpeed = 12.5f;
+
+    public float lowSpeed;
+
+    public float midSpeed;
+
+    public float highSpeed;
+
+    public float maxHighSpeed = 13.8f;
 
     // Max drag, if the player is on 0 deg or minAngle
     public float maxDrag = 6;
@@ -45,6 +50,13 @@ public class WingsuitController : MonoBehaviour
     // Audio mixer, to control the sound FX pitch
     public AudioMixer am;
 
+    //Decrease Valuse
+    public float decreaseNumber;
+
+    //gliders velocity value
+    public float Velocity;
+    public float Force;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -54,12 +66,17 @@ public class WingsuitController : MonoBehaviour
 
     private void Update()
     {
-        
+
+        Velocity = rb.velocity.magnitude;
+        Force = mod_force;
 
         //rotate the player
         //X axis
         rot.x += xRotation * Input.GetAxis("Vertical") * Time.deltaTime;
         rot.x = Mathf.Clamp(rot.x, minAngle, maxAngle);
+
+        //Gradual decreasing in angle on the X axis
+        rot.x -= +decreaseNumber * Time.deltaTime;
         //Y axis
         rot.y += yRotation * Input.GetAxis("Horizontal") * Time.deltaTime;
         //Clamped Z Axis rotation
@@ -78,17 +95,71 @@ public class WingsuitController : MonoBehaviour
         // If 0, we'll have maxDrag and lowSpeed
         // If 1, we'll get minDrag and highSpeed
         mod_drag = (percentage * (minDrag - maxDrag)) + maxDrag;
+
         mod_force = (percentage * (highSpeed - lowSpeed)) + lowSpeed;
+
+        SpeedChanges();
         // Getting the local space of the velocity
         Vector3 localV = transform.InverseTransformDirection(rb.velocity);
+
         // Change z velocity to mod_force
         localV.z = mod_force;
+
         // Convert the local velocity back to world space and set it to the Rigidbody's velocity
         rb.velocity = transform.TransformDirection(localV);
+
         // Update drag to the modified one
         rb.drag = mod_drag;
+
         // Change pitch value based on the player's angle and percentage
         am.SetFloat("Pitch", 1 + percentage);
 
+
+
+    }
+    public void SpeedChanges()
+    {
+
+        //Percentage Values
+        //-0.99 = straight up
+        //-0.5  = 45 degrees up
+        //0.0 = straight ahead
+        //0.5 = 45 degrees down
+        //0.9 = straight down
+
+        if (Input.GetButton("Vertical"))
+        {
+            decreaseNumber = 0;
+        }
+        else 
+        {
+            if (percentage >= 0.75f && percentage <= 1)
+            {
+                Debug.Log("Freefalling");
+                decreaseNumber = 0;
+            }
+            else if (percentage >= 0.1f && percentage <= 0.90f)
+            {
+                Debug.Log("Diving");
+                decreaseNumber = -5f;
+            }
+            else if (percentage <= 0.1 && percentage >= -0.1)
+            {
+                Debug.Log("Mid");
+                decreaseNumber = -1;
+            }
+            else if (percentage <= -0.1 && percentage >= -0.5f)
+            {
+                Debug.Log("Climbing");
+                decreaseNumber = Mathf.Lerp(rot.x, -1, -10f);
+            }
+            else if (percentage <= -0.5f && percentage >= -1)
+            {
+                Debug.Log("Stalling");
+                decreaseNumber = Mathf.Lerp(rot.x, -1, -10f);
+            }
+        }
+
+        //mod_force = (percentage * (highSpeed - lowSpeed)) + lowSpeed;
     }
 }
