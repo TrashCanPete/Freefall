6d7072c9aed27e2c9adfbbb0117a73a097abac99
Josh <joshuaeric.malcolm@gmail.com>
2020-04-09 14:43:49 +1200

    Shader update

--- Project/Assets/Scripts/Player/GliderController.cs
+++ Project/Assets/Scripts/Player/GliderController.cs
@@ -130,6 +130,10 @@ public class GliderController : MonoBehaviour
         {
             UpDraftCounter(_collison.Oxygen);
         }
+        if (other.tag == ("Finish"))
+        {
+            Application.Quit(1);
+        }
     }
 
     private void OnCollisionStay(Collision collision)
