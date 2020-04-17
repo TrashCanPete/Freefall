88580770f5b3edf9aec613544be21222b135407b
yungghiblie <dantekennedygeorge@outlook.com>
2020-04-17 19:34:43 +1200

    Little fix

--- Project/Assets/Scripts/CapeMovement.cs
+++ Project/Assets/Scripts/CapeMovement.cs
@@ -8,18 +8,14 @@ public class CapeMovement : MonoBehaviour
     public FlyingStates flyingStates;
     public Material capemovement;
     public float Windspeed;
+    
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        flyingStates = GetComponent < FlyingStates > ();
-        capemovement.SetFloat("Vector1_f973097f",Windspeed);
-    }
-
+   
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        _ = Windspeed == flyingStates.Speed;
-         
+
+        capemovement.SetFloat("Vector1_f973097f", Windspeed);
     }
 }
+
