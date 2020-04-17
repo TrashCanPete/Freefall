b6785cc728e3fd03f56bef07c96bff25bbc13de2
yungghiblie <dantekennedygeorge@outlook.com>
2020-04-17 19:16:22 +1200

    Auto stash before merge of "master" and "origin/master"

---
+++ Project/Assets/Scripts/CapeMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapeMovement : MonoBehaviour
+{
+
+    public FlyingStates flyingStates;
+    public Material capemovement;
+    public float Windspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flyingStates = GetComponent < FlyingStates > ();
+        capemovement.SetFloat("Vector1_f973097f",Windspeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _ = Windspeed == flyingStates.Speed;
+         
+    }
+}
