f26b0f4038d08996e396b848383a34183f2089ed
Peter <mcdermpete@gmail.com>
2020-04-22 10:34:33 +1200

    New Menu Scene set up called Main_Menu

--- Project/Assets/Scripts/MenuController.cs
+++ Project/Assets/Scripts/MenuController.cs
@@ -69,6 +69,6 @@ public class MenuController : MonoBehaviour
     }
     public void QuitToMenu()
     {
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Main_Menu");
     }
 }
