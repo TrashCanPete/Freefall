53f8b1906db3c71333476f444d4b683504d348ee
Peter <mcdermpete@gmail.com>
2020-04-24 12:50:07 +1200

    Added Post process, bird formations updated

--- Project/Assets/Scripts/Birds/FlockScript.cs
+++ Project/Assets/Scripts/Birds/FlockScript.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class FlockScript : MonoBehaviour
 {
-    public GameObject birdPrefab;
+    public GameObject birdPrefab1;
+    public GameObject birdPrefab2;
+    public GameObject birdPrefab3;
     public Transform target;
     public Transform target1;
     public Transform target2;
+    public Transform target3;
+    public Transform target4;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,9 +30,12 @@ public class FlockScript : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            Instantiate(birdPrefab, target.transform.position, Quaternion.identity);
-            Instantiate(birdPrefab, target1.transform.position, Quaternion.identity);
-            Instantiate(birdPrefab, target2.transform.position, Quaternion.identity);
+            Instantiate(birdPrefab1, target.transform.position, Quaternion.identity);
+            Instantiate(birdPrefab2, target1.transform.position, Quaternion.identity);
+            Instantiate(birdPrefab3, target2.transform.position, Quaternion.identity);
+            Instantiate(birdPrefab1, target3.transform.position, Quaternion.identity);
+            Instantiate(birdPrefab1, target4.transform.position, Quaternion.identity);
+            Destroy(this);
         }
     }
 }
